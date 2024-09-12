6232de00e53e498e940c3cd2add2f762d0b37522
Rob Mackert <noxrob.stream@gmail.com>
2024-09-12 12:45:11 -0500

    More training

---
+++ import_modules.py
@@ -0,0 +1,54 @@
+# #import import_example as mm
+# import sys
+
+# from import_example import find_index, test
+
+
+# courses=['History','Math', 'Physics', 'CompSci']
+
+# #index = mm.find_index(courses, 'Math')
+# index = find_index(courses, 'Math')
+# # print(index)
+# # print(test)
+
+# print(sys.path)
+
+
+
+# import random
+
+# courses=['History','Math', 'Physics', 'CompSci']
+# random_course = random.choice(courses)
+
+# print(random_course)
+
+
+
+# import math
+
+# courses=['History','Math', 'Physics', 'CompSci']
+
+# rads = math.radians(90)
+# print(rads)
+
+
+# import datetime
+# import calendar
+
+# courses=['History','Math', 'Physics', 'CompSci']
+
+# today = datetime.date.today()
+# print(today)
+
+# print(calendar.isleap(2020))
+
+
+
+import os
+
+courses=['History','Math', 'Physics', 'CompSci']
+
+print(os.getcwd)
+# get.cwd = get current working directory
+
+print(os.__file__)
---
+++ loops_iterations.py
@@ -0,0 +1,28 @@
+#nums = [1, 2, 3, 4, 5]
+
+#Loops for Break and Continue
+# for num in nums:
+#     if num == 3:
+#         print('Found!')
+#         continue (can also replace with break if you want to loop to stop once it found the num 3)
+#     print(num)
+
+#continous loop:
+
+# for num in nums:
+#     for letter in 'abc':
+#         print(num, letter)
+
+# for i in range(1, 11):
+#     print(i)
+
+
+x = 0
+
+while True:
+    # if x == 5:
+    #     break
+    print(x)
+    x += 1
+# if you have an infinate loop most OS will allow you to press CRTL + C to end the loop.
+
