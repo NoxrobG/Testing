785e0bafd4392e5626585dfb3feb9bcc70cf183a
Rob Mackert <noxrob.stream@gmail.com>
2024-09-11 20:41:04 -0500

    adding

---
+++ if_else.py
@@ -0,0 +1,70 @@
+
+# Comparisons:
+# Equal:            ==
+# Not Equal:        !=
+# Greater Than:     >
+# Less Than:        <
+# Greater or Equal: >=
+# Less or Equal:    <=
+# Object Identity:  is
+
+
+
+#language = 'Java'
+
+# if language == 'Python':
+#     print('Language is Python')
+# elif language == 'Java':
+#     print('Language is Java')
+# elif language == 'JavaScript':
+#     print('Language is JavaScript')
+# else:
+#     print('No Match')
+
+
+#and
+#or
+#not
+
+# user = 'Admin'
+# logged_in = False
+
+# # if user =="Admin" or logged_in:
+# #     print('Admin Page')
+# # else:
+# #     print('Bad Creds')
+
+# if not logged_in:
+#     print('Please log in')
+# else:
+#     print('Welcome')
+
+# a = [1, 2, 3]
+# #b = [1, 2, 3]
+# b = a
+
+
+# print(id(a))
+# print(id(b))
+# print(a is b)
+
+
+# False Values:
+    # False
+    # None
+    # Zero of any numeric type
+    # Any empty sequence. For example, '', (), [].
+    # Any empty mapping. For example, {}.
+
+#condition = False
+#condition = None
+#condition = 0
+#condition = []
+condition = {}
+
+if condition:
+    print('Evaluated to True')
+else:
+    print('Evaluated to False')
+
+#Evaluated to False
